1e06e9bbf6e471e140e6f6ce7065cb843e7db53f
Jackson Hayward <jacksonhayward@Jacksons-MacBook-Pro.local>
2024-02-05 16:40:38 -0800

    Update arguments and add string method

--- scripts/classes.py
+++ scripts/classes.py
@@ -1,15 +1,22 @@
 class Course:
-    def __init__(self, title, link, trip_date, registration_open_date, registration_close_date, branch):
+    def __init__(self, title, link, trip_date, registration_status, branch, difficulty):
         self.title = title
         self.link = link
         self.trip_date = trip_date
-        self.registration_open_date = registration_open_date
-        self.registration_close_date = registration_close_date
+        self.registration_status = registration_status
         self.branch = branch
+        self.difficulty = difficulty
+
+    def __str__(self):
+        return (f'Trip name:                {self.title}\n' +
+                f'Trip date:                {self.trip_date}\n' +
+                f'Trip registration status: {self.registration_status}\n' +
+                f'Branch:                   {self.branch}\n' +
+                f'Trip difficulty:          {self.difficulty}\n'
+                f'Trip webpage:             {self.link}\n')
 
 
 class User:
     def __init__(self, email, list_of_keywords):
         self.email = email
         self.keyword_list = list_of_keywords
-
