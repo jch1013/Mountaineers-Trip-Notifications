b019fddee6bceb571f87423d351b5da7b7d4ac7e
Jackson Hayward <jacksonhayward@Jacksons-MacBook-Pro.local>
2024-02-06 16:05:49 -0800

    Finish scraper

--- scripts/scrape_courses.py
+++ scripts/scrape_courses.py
@@ -6,12 +6,17 @@ import time
 import bs4
 
 
-# returns a list of current courses being offererd with the given category
+# returns a list of current courses being offered with the given category
 def search(keyword):
     total_results = get_total_results(keyword)
     total_pages = math.ceil(total_results / 20)
 
     driver = webdriver.Chrome()
+
+    # list of trips to return
+    trips = []
+
+    # For each page of results, and for each result on each page, scrape trip and create a trip object
     for num in range(total_pages):
         page_to_scrape = f'https://www.mountaineers.org/activities/activities#b_start={20 * num}&c4={keyword}'
         driver.get(page_to_scrape)
@@ -26,17 +31,28 @@ def search(keyword):
 
         for i in range(len(trip_names)):
             name = trip_names[i].text.strip()
-            link = "link"
             date = trip_dates[i].text.strip()
             registration_status = trip_registration_statuses[i].text.strip()
             branch = trip_branches[i].text.strip()
-            difficulty = "difficulty"
+
+            try:
+                difficulty = trip_difficulties[i].text.strip()
+            except IndexError:
+                difficulty = "Not listed"
+
+            try:
+                link = trip_names[i].find('a')['href']
+            except KeyError:
+                link = "Webpage coming soon"
+
             trip = classes.Course(name, link, date, registration_status, branch, difficulty)
-            print(trip)
+
+            trips.append(trip)
+
+    return trips
 
 
-
-
+# Function that determines how many pages of trips there are to scrape
 def get_total_results(keyword):
     first_page_results = f'https://www.mountaineers.org/activities/activities#b_start=0&c4={keyword}'
     driver = webdriver.Chrome()
